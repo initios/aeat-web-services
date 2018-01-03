3f00e8c6240f37c19040975bfb11045ce76a7f27
Carlos Goce <carlosgoce@gmail.com>
2018-01-03 18:53:39 +0100

    Updated about

--- aeat/__about__.py
+++ aeat/__about__.py
@@ -1,6 +1,6 @@
 
 __title__ = 'aeat-web-services'
-__summary__ = 'Integración con Agencia Estatal de Administración Tributaria a través de sus servicios web'
+__summary__ = 'Integración con la Agencia Estatal de Administración Tributaria'
 __version__ = '1.0.0-dev'
 __license__ = 'MIT'
 __uri__ = 'https://github.com/initios/aeat-web-services'
