a5c5a09f8d75dfadceb58075f4968165777295de
Carlos Goce <carlosgoce@gmail.com>
2018-01-03 18:31:35 +0100

    Adds __about__.py and install_requires to setup.py

---
+++ aeat/__about__.py
@@ -0,0 +1,8 @@
+
+__title__ = 'aeat-web-services'
+__summary__ = 'Integración con Agencia Estatal de Administración Tributaria a través de sus servicios web'
+__version__ = '1.0.0-dev'
+__license__ = 'MIT'
+__uri__ = 'https://github.com/initios/aeat-web-services'
+__author__ = 'Initios Desarrollos'
+__email__ = 'dev@initios.com'
---
+++ setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# Copyright 2018 Initios Desarrollos
+#
+# All rights reserved
+
+import os
+import setuptools
+
+
+base_dir = os.path.dirname(__file__)
+
+about = {}
+
+with open(os.path.join(base_dir, 'aeat', '__about__.py')) as f:
+    exec(f.read(), about)
+
+with open(os.path.join(base_dir, 'README.rst')) as readme:
+    long_description = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+# https://packaging.python.org/tutorials/distributing-packages/
+
+setuptools.setup(
+    name=about['__title__'],
+    version=about['__version__'],
+
+    description=about['__summary__'],
+    long_description=long_description,
+    license=about['__license__'],
+    url=about['__uri__'],
+
+    author=about['__author__'],
+    author_email=about['__email__'],
+
+    install_requires=[
+        'lxml>=4.1.1,<4.2.0',
+        'xmlsec>=1.3.3,<1.4.0',
+        'zeep>=2.4.0,<2.5.0',
+    ]
+
+    python_requires='~=3.6',
+
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+    ],
+
+    packages=['aeat'],
+)
