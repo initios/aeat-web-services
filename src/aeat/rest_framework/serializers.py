cd81979a0de3ad77fc6bd7dea26754617a3dc054
Carlos Goce <carlosgoce@gmail.com>
2018-01-10 11:32:14 +0100

    Remove ValidationError

--- src/aeat/rest_framework/serializers.py
+++ src/aeat/rest_framework/serializers.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 from rest_framework import serializers as rf
-from rest_framework.serializers import ValidationError
 
 import aeat
 
