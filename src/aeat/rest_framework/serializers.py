e1aa63ba05a20d875ccb67cc40b7c81cab6633fc
Carlos Goce <carlosgoce@gmail.com>
2018-01-11 14:56:03 +0100

    Add test mrn serializer

--- src/aeat/rest_framework/serializers.py
+++ src/aeat/rest_framework/serializers.py
@@ -1,1 +1,26 @@
 from rest_framework import serializers as rf
+
+
+def lxml_to_dict(node):
+    """
+    Convert an lxml.etree node tree into a dict.
+    """
+    storage = {}
+
+    for e in node.iterchildren():
+        key = e.tag.split('}')[1] if '}' in e.tag else e.tag
+        storage[key] = e.text if e.text else lxml_to_dict(e)
+
+    return storage
+
+
+class DequeToDictMixin:
+    def to_internal_value(self, data):
+        return {item.tag: lxml_to_dict(item) for item in data}
+
+
+class MRNSerializer(DequeToDictMixin, rf.Serializer):
+    mrn = rf.SerializerMethodField()
+
+    def get_mrn(self, obj):
+        return obj['HEAHEA']['DocNumHEA5']
---
+++ tests/test_serializers.py
@@ -0,0 +1,14 @@
+import setup_django  # NOQA
+
+from aeat.rest_framework import serializers
+
+
+def test_mrn_serializer(zeep_response):
+    aeat_response = zeep_response(
+        'wsdl_ens_presentation_IE315V4.wsdl', 'ens_presentation_success_IE328V4Sal.xml', 'IE315V4'
+    )
+
+    serializer = serializers.MRNSerializer(data=aeat_response)
+    assert serializer.is_valid(raise_exception=False), serializer.errors
+
+    assert {'mrn': '17ES004311Z0000010'} == serializer.data
