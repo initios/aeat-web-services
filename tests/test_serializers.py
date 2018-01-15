1296788149655d04e8530f81aaddab8cf1b2bfe0
Carlos Goce <carlosgoce@gmail.com>
2018-01-15 12:37:42 +0100

    Add test XSD

--- tests/test_serializers.py
+++ tests/test_serializers.py
@@ -30,21 +30,20 @@ def test_exs_serializer(zeep_response):
 
 @pytest.mark.parametrize('url,response,operation,expected,is_error,expected_data', [
     # ENS
-    # ('wsdl_ens_presentation_IE315V4.wsdl', 'ens_presentation_success_IE328V4Sal.xml', 'IE315V4',
-    #  serializers.ENSSerializer, False, {'mrn': '17ES004311Z0000010'}),
+    ('wsdl_ens_presentation_IE315V4.wsdl', 'ens_presentation_success_IE328V4Sal.xml', 'IE315V4',
+     serializers.ENSSerializer, False, {'mrn': '17ES004311Z0000010'}),
 
-    # ('wsdl_ens_presentation_IE315V4.wsdl', 'ens_presentation_error_IE316V4Sal.xml', 'IE315V4',
-    #  serializers.ENSFunctionalErrorSerializer, True,
-    #  {'type': '12', 'pointer': 'MES.MesSenMES3', 'reason': '1234-Message Sender is not valid'}),
+    ('wsdl_ens_presentation_IE315V4.wsdl', 'ens_presentation_error_IE316V4Sal.xml', 'IE315V4',
+     serializers.ENSFunctionalErrorSerializer, True,
+     {'type': '12', 'pointer': 'MES.MesSenMES3', 'reason': '1234-Message Sender is not valid'}),
 
     # # EXS
     ('wsdl_exs_IE615V1.wsdl', 'exs_presentation_success_IE628V1Sal.xml', 'IE615V1',
      serializers.EXSSerializer, False,
      {'mrn': '17ES00361160001234', 'customs_intervention_code': 'V', 'item_number_involved': 0}),
 
-    # ('wsdl_exs_IE615V1.wsdl', 'exs_presentation_error_IE919V1Sal.xml', 'IE615V1',
-    #  serializers.UnknownResponseSerializer, True, {'reason': 'Unknown AEAT response'}),
-
+    ('wsdl_exs_IE615V1.wsdl', 'exs_presentation_error_IE919V1Sal.xml', 'IE615V1',
+     serializers.UnknownResponseSerializer, True, {'reason': 'Unknown AEAT response'}),
 ])
 def test_get_serializer_for_mapped_response(zeep_response, url, response, operation, expected,
                                             is_error, expected_data):
@@ -66,3 +65,13 @@ def test_get_serializer_for_unmapped_response():
     serializer = serializer_cls(data=aeat_response)
     assert serializer.is_valid(), serializer.errors
     assert {'reason': 'Unknown AEAT response'} == serializer.data
+
+
+@pytest.mark.parametrize('filename,expected', [
+    ('exs_presentation_success_IE628V1Sal.xml',
+     'https://www2.agenciatributaria.gob.es/ADUA/internet/es/aeat/dit/adu/adrx/ws/IE628V1Sal.xsd')
+])
+def test_parse_xsd(response_etree_element, filename, expected):
+    xml = response_etree_element(filename)
+    print(xml)
+    assert expected == serializers.parse_xsd(xml)
