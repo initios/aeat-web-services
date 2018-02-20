5f019332f013bea576c008ac8d7b380eaeae0617
Carlos Goce <carlosgoce@gmail.com>
2018-02-20 14:37:34 +0100

    Update version to 2.0.0

--- src/aeat/__about__.py
+++ src/aeat/__about__.py
@@ -1,7 +1,7 @@
 
 __title__ = 'aeat-web-services'
 __summary__ = 'Spanish Tax Agency Electronic Office (AEAT) Integration'
-__version__ = '2.0.0-dev-1'
+__version__ = '2.0.0'
 __license__ = 'MIT'
 __uri__ = 'https://github.com/initios/aeat-web-services'
 __author__ = 'Initios Desarrollos'
