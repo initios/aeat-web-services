e05e180ccc82bcdda6cc215e1d32f01eac54b833
Carlos Goce <carlosgoce@gmail.com>
2018-02-20 11:43:27 +0100

    update tests

--- tests/test_xml_signing.py
+++ tests/test_xml_signing.py
@@ -6,30 +6,30 @@ from aeat import xml_signing
 def test_sign_without_root_id(certificate_example):
     root = etree.Element('Data')
 
-    xml_signing.sign(root, *certificate_example)
+    signed = xml_signing.sign(root, *certificate_example)
 
-    assert xml_signing.verify(root, *certificate_example)
-    assert 'MessageRoot' == root.attrib['Id']
+    assert xml_signing.verify(signed, *certificate_example)
+    assert 'MessageRoot' == signed.attrib['Id']
 
 
 def test_verify_simple_signature(certificate_example):
     root = etree.Element('Data', Id='myid')
-    xml_signing.sign(root, *certificate_example)
+    signed = xml_signing.sign(root, *certificate_example)
 
-    assert xml_signing.verify(root, *certificate_example)
+    assert xml_signing.verify(signed, *certificate_example)
 
 
 def test_verify_wrong_signature(certificate_example):
     root = etree.Element('Data', Id='myid')
 
-    xml_signing.sign(root, *certificate_example)
-    etree.SubElement(root, 'KeyAfterSigning')
+    signed = xml_signing.sign(root, *certificate_example)
+    etree.SubElement(signed, 'KeyAfterSigning')
 
-    assert xml_signing.verify(root, *certificate_example) is False
+    assert xml_signing.verify(signed, *certificate_example) is False
 
 
 def test_sign_complete_xades_epel(certificate_example):
     root = etree.Element('Data', Id='myid')
 
-    xml_signing.sign(root, *certificate_example)
-    assert xml_signing.verify(root, *certificate_example)
+    signed = xml_signing.sign(root, *certificate_example)
+    assert xml_signing.verify(signed, *certificate_example)
