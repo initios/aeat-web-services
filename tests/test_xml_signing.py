8fa109f2ae56648d629cd9afd0c1fe92b2afe8f0
Carlos Goce <carlosgoce@gmail.com>
2018-02-20 10:12:29 +0100

    Refactor tests

--- tests/test_xml_signing.py
+++ tests/test_xml_signing.py
@@ -20,20 +20,16 @@ def test_verify_simple_signature(certificate_example):
 
 
 def test_verify_wrong_signature(certificate_example):
-    root = etree.Element('Envelope')
-    body = etree.SubElement(root, 'Body')
-    data = etree.SubElement(body, 'Data', Id='myid')
+    root = etree.Element('Data', Id='myid')
 
     xml_signing.sign(root, *certificate_example)
-    etree.SubElement(data, 'KeyAfterSigning')
+    etree.SubElement(root, 'KeyAfterSigning')
 
     assert xml_signing.verify(root, *certificate_example) is False
 
 
 def test_sign_complete_xades_epel(certificate_example):
-    root = etree.Element('Envelope')
-    body = etree.SubElement(root, 'Body')
-    etree.SubElement(body, 'Data', Id='myid')
+    root = etree.Element('Data', Id='myid')
 
     xml_signing.sign(root, *certificate_example)
     assert xml_signing.verify(root, *certificate_example)
