61db403730ae8552b8757d9b3fe967626f888686
Carlos Goce <carlosgoce@gmail.com>
2018-02-21 12:05:24 +0100

    Adds WSDL to services mapping

--- src/aeat/services.py
+++ src/aeat/services.py
@@ -1,9 +1,11 @@
 from aeat.rest_framework import serializers
 from aeat.rest_framework import validators
+from aeat.wsdl import ADUANET_SERVICES
 
 
 class ENSPresentationService:
     service_name = 'ens_presentation'
+    wsdl = ADUANET_SERVICES['ens_presentation']  # TODO
     serializer = serializers.ENSSerializer
     validator = validators.ENSPresentationValidator
     qname = '{https://www2.agenciatributaria.gob.es/ADUA/internet/es/aeat/dit/adu/aden/enswsv5/IE315V5Ent.xsd}CC315A'  # noqa
